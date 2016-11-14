dd07d95587475d4d2277990d85b2b44a30ae3ea8
traquall <le_d-JJ_39@hotmail.fr>
2016-11-14 17:04:36 +0100

    correction gapi....

--- js/search_keyword.js
+++ js/search_keyword.js
@@ -5,20 +5,23 @@ $(function(){
 	}
 
 	// Search for a specified string.
-	// function search() {
-	$('.form_search').submit(function(e){
-		e.preventDefault();
-	  var query = $('#query').val();
-	  var request = gapi.client.youtube.search.list({
-	    q: query,
-	    part: 'snippet'
-	  });
+	function search() {
+	//$('.form_search').submit(function(e){
+	//e.preventDefault();
+		gapi.client.setApiKey('AIzaSyARvwirFktEIi_BTaKcCi9Ja-m3IEJYIRk');
+        gapi.client.load('youtube', 'v3', function() {
+			  var query = $('#query').val();
+			  var request = gapi.client.youtube.search.list({
+			    q: query,
+			    part: 'snippet'
+			  });
 
-	  request.execute(function(response) {
-	    console.log(response);
-	    var str = JSON.stringify(response.result);
-	    console.log(str);
-	    $('#search-container').html(str);
-	  });
-	});
+			  request.execute(function(response) {
+			    console.log(response);
+			    var str = JSON.stringify(response.result);
+			    console.log(str);
+			    $('#search-container').html(str);
+			  });
+			});
+    }
 });
