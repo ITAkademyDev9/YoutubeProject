bc3eab7dcc8b2b80bc91435f87780a0104b1ac92
klenzo <crea2luxe@gmail.com>
2016-11-14 15:31:42 +0100

    ajout du script d'abonnement et recherche

---
+++ js/suscribe.js
@@ -0,0 +1,29 @@
+/**
+ * This sample subscribes the active user to the GoogleDevelopers
+ * YouTube channel, specified by the channelId.
+ */
+function addSubscription(channelId) {
+  // Replace this channel ID with the channel ID you want to subscribe to
+  
+  // var channelId = 'UC9gFih9rw0zNCK3ZtoKQQyA';
+  var channelId = channelId;
+  var resource = {
+    snippet: {
+      resourceId: {
+        kind: 'youtube#channel',
+        channelId: channelId
+      }
+    }
+  };
+
+  try {
+    var response = YouTube.Subscriptions.insert(resource, 'snippet');
+    Logger.log(response);
+  } catch (e) {
+    if(e.message.match('subscriptionDuplicate')) {
+      Logger.log('Cannot subscribe; already subscribed to channel: ' + channelId);
+    } else {
+      Logger.log('Error adding subscription: ' + e.message);
+    }
+  }
+}
