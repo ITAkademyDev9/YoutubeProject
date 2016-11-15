06d92cbca83eba3ae7c962d9e6c0bb7e549f9843
klenzo <crea2luxe@gmail.com>
2016-11-15 09:59:57 +0100

    V2 - Modifs

--- js/search.js
+++ js/search.js
@@ -14,14 +14,18 @@ $(function(){
 
 
     function searchA() {
-            var q = $('#query').val();
+            var query = $('#query').val();
             var request = gapi.client.youtube.channels.list({
-                    part: 'videos'
+                q: query,
+                part: 'snippet',
             });
             request.execute(function(response) {
                 var str = JSON.stringify(response.result);
-                $('.result').html(response);
+                $('#result').append(response);
                 console.log(response);
             });
     }
 });
+
+
+// 7lCDEYXw3mM
